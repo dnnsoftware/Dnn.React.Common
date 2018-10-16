8443d04593a88990503a20b4ca95ada8edf5b25b
Matt Rutledge <mattrutledge@believegroup.com>
2018-10-16 13:01:45 -0600

    Update BuildAll and install-all to yarn

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -1,9 +1,15 @@
 Push-Location
 try {
 	get-childitem -Directory | foreach {
-		Write-Host "============ Building Component *** $_ *** ============"
+		Write-Host "============ Restoring Components *** $_ *** ============"
 		cd $_
 		yarn install
+		cd ..
+	}
+
+	get-childitem -Directory | foreach {
+		Write-Host "============ Building Component *** $_ *** ============"
+		cd $_
 		yarn run prepublishOnly
 		cd ..
 	}
--- install-all.js
+++ install-all.js
@@ -3,16 +3,16 @@ const fs = require("fs");
 const child_process = require("child_process");
 
 const root = process.cwd(); 
-npm_install_recursive(root);
+yarn_install_recursive(root);
 
 // Since this script is intended to be run as a "preinstall" command,
-// it will do `npm install` automatically inside the root folder in the end.
+// it will do `yarn install` automatically inside the root folder in the end.
 console.log("===================================================================");
-console.log("Performing \"npm install\" inside root folder");
+console.log("Performing \"yarn install\" inside root folder");
 console.log("===================================================================");
 
 // Recurses into a folder
-function npm_install_recursive(folder)
+function yarn_install_recursive(folder)
 {
     const has_package_json = fs.existsSync(path.join(folder, "package.json"));
 
@@ -22,32 +22,32 @@ function npm_install_recursive(folder)
         return;
     }
 
-    // If there is `package.json` in this folder then perform `npm install`.
+    // If there is `package.json` in this folder then perform `yarn install`.
     //
     // Since this script is intended to be run as a "preinstall" command,
-    // skip the root folder, because it will be `npm install`ed in the end.
+    // skip the root folder, because it will be `yarn install`ed in the end.
     // Hence the `folder !== root` condition.
     //
     if (has_package_json && folder !== root)
     {
         console.log("===================================================================");
-        console.log(`Performing "npm install" inside ${folder === root ? "root folder" : "./" + path.relative(root, folder)}`);
+        console.log(`Performing "yarn install" inside ${folder === root ? "root folder" : "./" + path.relative(root, folder)}`);
         console.log("===================================================================");
 
-        npm_install(folder);
+        yarn_install(folder);
     }
 
     // Recurse into subfolders
     for (let subfolder of subfolders(folder))
     {
-        npm_install_recursive(subfolder);
+        yarn_install_recursive(subfolder);
     }
 }
 
-// Performs `npm install`
-function npm_install(where)
+// Performs `yarn install`
+function yarn_install(where)
 {
-    child_process.execSync("npm install", { cwd: where, env: process.env, stdio: "inherit" });
+    child_process.execSync("yarn install", { cwd: where, env: process.env, stdio: "inherit" });
 }
 
 // Lists subfolders in a folder
